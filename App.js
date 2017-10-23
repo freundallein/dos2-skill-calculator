c83ccc8eac19e2a491193c46aebfbf2dad4ce54a
S.Zhavoronkov <zhavoronkov@vistgroup.ru>
2017-10-23 14:03:45 +0300

    add descriptions

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ export default class App extends React.Component {
         this.state = {
             char: character,
             modal: false,
-            description: 'Test'
+            description: ''
         };
     }
 
